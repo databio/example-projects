9b1865b2c9be33d2084812bb5ff8b24f56c557f8
Aaron Gu <32209687+aaron-gu@users.noreply.github.com>
2018-05-08 16:00:39 -0400

    Update rundeseq.R

--- rna-seq/rundeseq.R
+++ rna-seq/rundeseq.R
@@ -1,9 +1,9 @@
-source("DESeq-Packager.R")
+source("PATH/TO/.../DESeq-Packager.R")
 if(!requireNamespace("devtools"))
     install.packages("devtools", dependencies=TRUE)
 if(!requireNamespace("pepr"))
   devtools::install_github("pepkit/pepr", dependencies=TRUE)
-yaml <- "project_config.yaml"
-p <- pepr::Project(file = yaml)
+
+p <- pepr::Project(file = "project_config.yaml")
 countDataSet <- DESeq_Packager(p, "result_source", "target_id", "est_counts")
 save("countDataSet", file=".RData")
